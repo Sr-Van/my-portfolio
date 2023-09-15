03f6f354a3e92b591168c183be4e2899dc69dc12
Sr-Van <vanderson.dev1@gmail.com>
2023-09-15 18:33:58 -0300

    feat: i18n

--- src/js/translate.js
+++ src/js/translate.js
@@ -6,11 +6,40 @@ const projectsLink = document.getElementById('projects-a')
 const contactLink = document.getElementById('contact-a')
 const langLabel = document.getElementById('lang-a')
 
+const getElByClass = el => document.querySelector(el)
+
+const firstTitle = getElByClass('.first-title')
+const firstSubTitle = getElByClass('.first-subtitle')
+const paragraphSub = getElByClass('.paragraph-sub')
+const CardTextSkills = getElByClass('.card-text-skills')
+const CardTextSchooling = getElByClass('.card-text-schooling')
+const paragraphSchooling = getElByClass('.paragraph-schooling')
+const liTextSoftSkills1 = getElByClass('.li-soft-skills-text-1')
+const liTextSoftSkills2 = getElByClass('.li-soft-skills-text-2')
+const liTextSoftSkills3 = getElByClass('.li-soft-skills-text-3')
+const liTextSoftSkills4 = getElByClass('.li-soft-skills-text-4')
+const liTextSoftSkills5 = getElByClass('.li-soft-skills-text-5')
+const liTextSoftSkills6 = getElByClass('.li-soft-skills-text-6')
+
+const titleProjects = getElByClass('.title-projects')
+const ProjectParagraph1 = getElByClass('.project-paragraph-1')
+const ProjectParagraph2 = getElByClass('.project-paragraph-2')
+const ProjectParagraph3 = getElByClass('.project-paragraph-3')
+const ProjectParagraph4 = getElByClass('.project-paragraph-4')
+
+
+const titleContact = getElByClass('.title-contact')
+const labelContact1 = getElByClass('.label-contact-1')
+const labelContact3 = getElByClass('.label-contact-3')
+const btnSendSubmit = getElByClass('.btn-send-submit')
+
+
+
 const checkLangPt = document.querySelector('#flexSwitchCheckChecked')
 
 
 
-const showData = data => {
+const translateData = data => {
 
     const language = checkLangPt.checked ? 'pt' : 'en'
 
@@ -22,12 +51,34 @@ const showData = data => {
     contactLink.textContent = translate.Contact
     langLabel.textContent = translate.Lang
 
+    firstTitle.textContent = translate.firstTitle
+    firstSubTitle.textContent = translate.firstSubTitle
+    paragraphSub.textContent = translate.paragraphSub
+
+    CardTextSkills.textContent = translate.CardTextSkills
+    CardTextSchooling.textContent = translate.CardTextSchooling
+    paragraphSchooling.textContent = translate.paragraphSchooling
+    liTextSoftSkills1.textContent = translate.liTextSoftSkills1
+    liTextSoftSkills2.textContent = translate.liTextSoftSkills2
+    liTextSoftSkills3.textContent = translate.liTextSoftSkills3
+    liTextSoftSkills4.textContent = translate.liTextSoftSkills4
+    liTextSoftSkills5.textContent = translate.liTextSoftSkills5
+    liTextSoftSkills6.textContent = translate.liTextSoftSkills6
+
+    titleProjects.textContent = translate.TitleProjects
+    ProjectParagraph1.textContent = translate.ProjectParagraph1
+    ProjectParagraph2.textContent = translate.ProjectParagraph2
+    ProjectParagraph3.textContent = translate.ProjectParagraph3
+    ProjectParagraph4.textContent = translate.ProjectParagraph4
     
-    
+    titleContact.textContent = translate.titleContact
+    labelContact1.textContent = translate.labelContact1
+    labelContact3.textContent = translate.labelContact3
+    btnSendSubmit.textContent = translate.btnSendSubmit
 }
 
 const getData = async () => {
-    await fetch(`/data.json`).then(response => response.json().then(data => showData(data)))
+    await fetch(`/data.json`).then(response => response.json().then(data => translateData(data)))
 }
 
 langDiv.addEventListener('click', () => {
